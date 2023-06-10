37a2445d9137ef9b4ea1a16f498417991d65d8d8
Mhdseomid <129630305+Mhdseomid@users.noreply.github.com>
2023-06-10 21:46:22 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,19 @@
-# test_negative.py
+# test_subtract.py
 
 import unittest
-from negative import Negative
+from subtract import subtract_numbers
 
-class TestNegative(unittest.TestCase):
-    def setUp(self):
-        self.negative = Negative()
+class TestSubtract(unittest.TestCase):
 
-    def test_negative_one(self):
-        result = self.negative.negate(1)
-        self.assertEqual(result, -1)
+    def test_subtract_positive_numbers(self):
+        self.assertEqual(subtract_numbers(5, 3), 2)
+        self.assertEqual(subtract_numbers(10, 5), 5)
+        self.assertEqual(subtract_numbers(0, 5), -5)
 
-    def test_negative_zero(self):
-        result = self.negative.negate(0)
-        self.assertEqual(result, 0)
-
-    def test_negative_negative(self):
-        result = self.negative.negate(-5)
-        self.assertEqual(result, 5)
-
-    def test_negative_float(self):
-        result = self.negative.negate(3.14)
-        self.assertEqual(result, -3.14)
+    def test_subtract_negative_numbers(self):
+        self.assertEqual(subtract_numbers(-2, -3), 1)
+        self.assertEqual(subtract_numbers(-5, -10), 5)
+        self.assertEqual(subtract_numbers(-2, 4), -6)
 
 if __name__ == '__main__':
     unittest.main()
